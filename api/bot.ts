40a9261f1686ab833f21bb409d7e1915b175ea6c
Mahdi Sheibak <mahdi.zoraghi.sheibak@gmail.com>
2024-11-01 06:30:43 -0700

    add express

--- api/bot.ts
+++ api/bot.ts
@@ -1,4 +1,5 @@
 import { Bot, webhookCallback } from "grammy";
+import express from "express";
 
 const token = process.env.TOKEN;
 if (!token) throw new Error("TOKEN is unset");
@@ -24,6 +25,22 @@ bot.on("message", (ctx) => {
 //     console.log({ error });
 //   });
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+app.get("/api", (req, res) => {
+  res.send("/api Express on Vercel");
+});
+// app.get("/api/bot", (req, res) => res.send("/api Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+app.use(webhookCallback(bot, "express"));
+
+module.exports = app;
+
 // export default webhookCallback(bot, "std/http");
 // export default webhookCallback(bot, "http");
-export default webhookCallback(bot, "express");
+// export default webhookCallback(bot, "express");
